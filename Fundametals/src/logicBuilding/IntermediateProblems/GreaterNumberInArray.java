6f2327edbcda8e11bdfb80e93b4848c680e92134
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-21 03:30:52 +0500

    changes

--- Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
+++ Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
@@ -3,5 +3,6 @@ package logicBuilding.IntermediateProblems;
 public class GreaterNumberInArray {
     public static void main(String[] args) {
         System.out.println("lets solve this problem");
+        System.out.println("Actually i am learning git as well and creating a repository for it to store my data");
     }
 }
