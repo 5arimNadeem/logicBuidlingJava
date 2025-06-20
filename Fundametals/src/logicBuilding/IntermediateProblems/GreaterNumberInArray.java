d8a3b361bea9d29096e8593a7ed9980442dcb1d0
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-21 03:28:27 +0500

    primeNumber login in easy problems file

---
+++ Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
@@ -0,0 +1,7 @@
+package logicBuilding.IntermediateProblems;
+
+public class GreaterNumberInArray {
+    public static void main(String[] args) {
+        System.out.println("lets solve this problem");
+    }
+}
