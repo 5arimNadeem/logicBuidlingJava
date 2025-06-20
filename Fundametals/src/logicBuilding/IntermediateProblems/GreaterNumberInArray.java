55ce2bd14619e406d4369dc6ce15d531b825b42c
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-21 03:35:15 +0500

    primeNumber login in easy problems file

--- Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
+++ Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
@@ -2,9 +2,6 @@ package logicBuilding.IntermediateProblems;
 
 public class GreaterNumberInArray {
     public static void main(String[] args) {
-        System.out.println("lets solve this problem");
-        System.out.println("Actually i am learning git as well and creating a repository for it to store my data");
 
-        System.out.println("people out there have made github complex like i don't know what it really is but i studied it from nick white and he made it easy for me.");
     }
 }
