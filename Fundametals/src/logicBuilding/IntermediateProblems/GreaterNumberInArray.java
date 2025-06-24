211ecfd51c75e3129eed904d53ea6adf52013fa1
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-25 00:59:41 +0500

    madeAnUpdate

--- Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
+++ Fundametals/src/logicBuilding/IntermediateProblems/GreaterNumberInArray.java
@@ -2,21 +2,30 @@ package logicBuilding.IntermediateProblems;
 
 public class GreaterNumberInArray {
     public static void main(String[] args) {
-        int arr[]= {2,4,5};
+        int arr[]= {2,4,5,23,54,90};
 
         if(arr.length == 0) System.out.println("Array is empty");
-
-//        else if(arr.length > 0){
-//            for (int i = 0; i < arr.length; i++) {
-//                System.out.println(arr[i]);
-                if(arr[0] == arr[1] && arr[0]== arr[2]){
-                    System.out.println("The numbers are equal");
-                } else if(arr[0] > arr[1] && arr[0] > arr[2]){
-                    System.out.println(arr[0] + " is greateest number");
-                } else{
-                    System.out.println("There is no greater number");
-                }
-//            }
-//        }
+        
+        int max1;
+        int max2;
+        
+        if(arr[0] > arr[1]){
+            max1 = arr[0];
+            max2 = arr[1];
+        }else{
+            max1 = arr[1];
+            max2 = arr[0];
+        }
+        
+        for(int i = 2; i < arr.length; i++){
+            if(arr[i] > max1){
+                max2 = max1;
+                max1 = arr[i];
+//                System.out.println(max1 = arr[i]);
+            } else if (arr[i] > max2) {
+                max2 = arr[i];
+//                System.out.println(max2 = arr[i]);
+            }
+        }
     }
 }
