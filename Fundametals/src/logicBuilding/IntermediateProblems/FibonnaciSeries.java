4ac7cc22ca44fee50aeee718c4da29e6bfc3640e
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-29 18:46:44 +0500

    fibonnaci

--- Fundametals/src/logicBuilding/IntermediateProblems/FibonnaciSeries.java
+++ Fundametals/src/logicBuilding/IntermediateProblems/FibonnaciSeries.java
@@ -1,5 +1,20 @@
 package logicBuilding.IntermediateProblems;
 
 public class FibonnaciSeries {
+    public static void main(String[] args) {
+        int firstNum = 0, secondNum = 1, nextNum;
 
+        int seriesOf = 20;
+
+        for(int i = 1; i <= seriesOf; i++) {
+            System.out.println("First Number is " + firstNum);
+
+            nextNum = firstNum + secondNum;
+
+            firstNum = secondNum;
+            secondNum = nextNum;
+            
+
+        }
+    }
 }
