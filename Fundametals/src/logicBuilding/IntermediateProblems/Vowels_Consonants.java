071bded10cc267caf6a875b6a3364656344edaf7
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-29 18:31:32 +0500

    updated

--- Fundametals/src/logicBuilding/IntermediateProblems/Vowels_Consonants.java
+++ Fundametals/src/logicBuilding/IntermediateProblems/Vowels_Consonants.java
@@ -1,9 +1,29 @@
 package logicBuilding.IntermediateProblems;
 
+import java.util.Scanner;
+
 public class Vowels_Consonants {
     public static void main(String[] args) {
-        String name = "SarimNadeem";
 
+        int v = 0, cn = 0;
+
+        String name = "Globalization";
+
+        name = name.toLowerCase();
+        int n = name.length();
+
+        for(int i = 0; i < n; i++) {
+           char c = name.charAt(i);
+
+           if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                v++;
+           }else{
+               cn++;
+           }
+        }
+
+        System.out.println("Vowels in a word: " + v);
+        System.out.println("Consonants in a word: " + cn);
 
     }
 }
