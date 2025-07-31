8734c99aa6caf5db85e4debe775022b158c4c7d3
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-07-31 12:53:44 +0500

    make the update

--- Fundametals/src/logicBuilding/EasyProblems/Practise/loopWorking/For.java
+++ Fundametals/src/logicBuilding/EasyProblems/Practise/loopWorking/For.java
@@ -3,15 +3,8 @@ package logicBuilding.EasyProblems.Practise.loopWorking;
 public class For {
 
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
-            int space = i - 1;
-            for (int j = 0; j < space; j++) {
-                System.out.print(" ");
-            }
-            System.out.println(i + " ");
-//            for (int j = 0; j <= i; j++) {
-//                System.out.print(j + " ");
-//            }
+        for (int i = 1; i != 10; i++) {
+            System.out.print(i + " , ");
         }
     }
 }
