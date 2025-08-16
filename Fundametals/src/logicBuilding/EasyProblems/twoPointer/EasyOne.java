e81fdf645756aa8b210665fe2dd1d2dbfb3db715
5arimNadeem <sarimnadeem141@gmail.com>
2025-08-16 19:23:25 +0500

    logicProblems

--- Fundametals/src/logicBuilding/EasyProblems/twoPointer/EasyOne.java
+++ Fundametals/src/logicBuilding/EasyProblems/twoPointer/EasyOne.java
@@ -1,21 +1,38 @@
 package logicBuilding.EasyProblems.twoPointer;
 
-public class EasyOne {
-    public static int[] sumOfTwoPairs(int[] pairSum , int targetSum){
-        int left = 0 , right = pairSum.length - 1;
+import java.util.HashMap;
 
-        while(left < right){
-            int currentSum = pairSum[left] + pairSum[right];
-            if(pairSum[left] == pairSum[right]){
-                return new int[]{left, right};
-            }
-            if (targetSum < currentSum) {
-                left++;
+public class EasyOne {
+//    public static int[] sumOfTwoPairs(int[] pairSum , int targetSum){
+//        int left = 0 , right = pairSum.length - 1;
+//
+//        while(left < right){
+//            int currentSum = pairSum[left] + pairSum[right];
+//            if(pairSum[left] == pairSum[right]){
+//                return new int[]{left, right};
+//            }
+//            if (targetSum < currentSum) {
+//                left++;
+//            }else{
+//                right--;
+//            }
+//        }
+//
+//        return new int [] {-1 , -1};
+//    }
+
+//    improving complexity by using hashmap
+    public static int[] sumofTwoPairs(int[] pairSum, int targetSum){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < pairSum.length ; i++) {
+            if(map.containsKey(targetSum - pairSum[i])){
+                return new int[]{ map.get(targetSum - pairSum[i]), i};
             }else{
-                right--;
+                map.put(pairSum[i] , i);
             }
         }
 
-        return new int [] {-1 , -1};
+        return new int[] {-1 , - 1};
     }
 }
